d1c683b04763db57df582c097519be63d09c7297
Nicola Greco <notsecurity@gmail.com>
2013-04-21 21:37:13 +0200

    fixing req/res

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 var port = process.env.PORT || 1234
   , express = require('express')
   , async = require('async')
+  , ejs = require("ejs")
   , http = require('http');
 
 var app = express()
   , server = app.listen(port)
   , io = require('socket.io').listen(server);
+
+app.configure(function () {
+  app.set("views", __dirname + "/static");
+  app.set("view engine", "ejs");
+  app.engine("html", ejs.renderFile);
+  app.use(express.bodyParser());
+  app.use(express.cookieParser("alskjald0q9udqokwdmqldiqud0woqijdklq09"));
+  app.use(express.session());
+  app.use(express.methodOverride());
+  app.use(express.static(__dirname + "/static"));
+  app.use(app.router);
+});
+
+app.get("/", function(req, res) {
+  console.log(res.render);
+  res.render("editor.html");
+});
+
 
 io.sockets.on('connection', function (socket) {
   socket.on('my other event', function (data) {
